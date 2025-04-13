eb3d490da3f64238e7fc1e34037523477bba21c2
Kenny <101839505+khui0@users.noreply.github.com>
2025-04-12 21:56:21 -0400

    feat: menubar

--- src/lib/components/menubar/menubar.svelte
+++ src/lib/components/menubar/menubar.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
-  import type { Snippet } from "svelte";
+  import dayjs from "dayjs";
+  import { onMount, type Snippet } from "svelte";
+  import TablerCircleFilled from "~icons/tabler/circle-filled";
   import MenubarItem from "./menubar-item.svelte";
 
-  import TablerCircleFilled from "~icons/tabler/circle-filled";
-
   let { children }: { children?: Snippet } = $props();
+
+  let time: string = $state("");
+
+  update();
+  
+  onMount(() => {
+    setInterval(update, 1000);
+  });
+
+  function update() {
+    time = dayjs().format("ddd MMM D h:mm A");
+  }
 </script>
 
 <menu class="flex h-8 w-full items-center bg-white/50 px-2 backdrop-blur-[50px] dark:bg-black/18">
@@ -12,4 +24,6 @@
   <MenubarItem type="name">Firefox</MenubarItem>
   <MenubarItem>File</MenubarItem>
   <MenubarItem>Edit</MenubarItem>
+  <div class="flex-1"></div>
+  <MenubarItem>{time}</MenubarItem>
 </menu>
