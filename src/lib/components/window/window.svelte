5c6ec0fb6a4944269c24822ff3fe125b0ffac405
Kenny <101839505+khui0@users.noreply.github.com>
2025-04-13 17:02:42 -0400

    feat: ignore handles when not focused

--- src/lib/components/window/window.svelte
+++ src/lib/components/window/window.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
   import type { Vector } from "./helpers.svelte";
-  import { stackOrder } from "./windows.svelte";
 
   let {
     id,
     position = { x: 0, y: 0 },
     size = { x: 400, y: 300 },
-    minSize = { x: 300, y: 50 },
+    minSize = { x: 300, y: 28 },
     children,
   }: {
     id: string;
@@ -24,42 +23,42 @@
   <!-- Top -->
   <div
     data-window-transform="0,-1"
-    class="handle absolute -top-1.25 right-1.25 left-1.25 h-2.5 cursor-ns-resize"
+    class="not-window-active:pointer-events-none absolute -top-1.25 right-1.25 left-1.25 h-2.5 cursor-ns-resize"
   ></div>
   <!-- Bottom -->
   <div
     data-window-transform="0,1"
-    class="handle absolute right-1.25 -bottom-1.25 left-1.25 h-2.5 cursor-ns-resize"
+    class="not-window-active:pointer-events-none absolute right-1.25 -bottom-1.25 left-1.25 h-2.5 cursor-ns-resize"
   ></div>
   <!-- Left -->
   <div
     data-window-transform="-1,0"
-    class="handle absolute top-1.25 bottom-1.25 -left-1.25 w-2.5 cursor-ew-resize"
+    class="not-window-active:pointer-events-none absolute top-1.25 bottom-1.25 -left-1.25 w-2.5 cursor-ew-resize"
   ></div>
   <!-- Right -->
   <div
     data-window-transform="1,0"
-    class="handle absolute top-1.25 -right-1.25 bottom-1.25 w-2.5 cursor-ew-resize"
+    class="not-window-active:pointer-events-none absolute top-1.25 -right-1.25 bottom-1.25 w-2.5 cursor-ew-resize"
   ></div>
   <!-- Top Left -->
   <div
     data-window-transform="-1,-1"
-    class="handle absolute -top-1.25 -left-1.25 h-4 w-4 cursor-nwse-resize"
+    class="not-window-active:pointer-events-none absolute -top-1.25 -left-1.25 h-4 w-4 cursor-nwse-resize"
   ></div>
   <!-- Top Right -->
   <div
     data-window-transform="1,-1"
-    class="handle absolute -top-1.25 -right-1.25 h-4 w-4 cursor-nesw-resize"
+    class="not-window-active:pointer-events-none absolute -top-1.25 -right-1.25 h-4 w-4 cursor-nesw-resize"
   ></div>
   <!-- Bottom Left -->
   <div
     data-window-transform="-1,1"
-    class="handle absolute -bottom-1.25 -left-1.25 h-4 w-4 cursor-nesw-resize"
+    class="not-window-active:pointer-events-none absolute -bottom-1.25 -left-1.25 h-4 w-4 cursor-nesw-resize"
   ></div>
   <!-- Bottom Right -->
   <div
     data-window-transform="1,1"
-    class="handle absolute -right-1.25 -bottom-1.25 h-4 w-4 cursor-nwse-resize"
+    class="not-window-active:pointer-events-none absolute -right-1.25 -bottom-1.25 h-4 w-4 cursor-nwse-resize"
   ></div>
 {/snippet}
 
@@ -70,7 +69,7 @@
   data-window-transform="0,0"
   data-window-min="{minSize.x},{minSize.y}"
 >
-  <div class="h-full w-full overflow-hidden flex flex-col">
+  <div class="flex h-full w-full flex-col overflow-hidden">
     {@render children()}
   </div>
   {@render handles()}
