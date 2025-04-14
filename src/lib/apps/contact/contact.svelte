e9fab6ca4f60e9bada4ad00fa346c561de77b382
Kenny <101839505+khui0@users.noreply.github.com>
2025-04-14 18:07:13 -0400

    feat: contact

--- src/lib/apps/contact/contact.svelte
+++ src/lib/apps/contact/contact.svelte
@@ -5,47 +5,51 @@
   import SimpleIconsLinkedin from "~icons/simple-icons/linkedin";
 </script>
 
-<section>
-  <Wordmark class="w-auto text-8xl" />
-  <p class="text-lg font-extralight tracking-tighter uppercase">CS @ SBU &mdash; New York</p>
-</section>
-<section class="flex flex-wrap items-center justify-between">
-  <a
-    class="focus-ring text-3xl font-thin tracking-tighter text-wrap break-all"
-    href="mailto:hello@kennyhui.dev"
+<div class="flex h-full flex-col gap-2.5 p-5 text-center">
+  <section class="mx-auto mt-10 text-start">
+    <Wordmark class="w-auto text-8xl" />
+    <p class="text-base-content/30 text-sm font-extralight uppercase">CS @ SBU &mdash; New York</p>
+  </section>
+  <section
+    class="[&>*:nth-child(even)]:text-base-content/50 grid flex-1 grid-cols-[4fr_6fr] items-center gap-2 text-sm [&>*:nth-child(even)]:justify-self-start [&>*:nth-child(odd)]:justify-self-end"
   >
-    hello@kennyhui.dev
-  </a>
-</section>
-<section class="mt-2 flex flex-wrap justify-center gap-2">
-  <a
-    class="btn"
-    aria-label="LinkedIn"
-    href="https://www.linkedin.com/in/k-hui/"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <SimpleIconsLinkedin></SimpleIconsLinkedin>
-    linkedin.com/in/k-hui
-  </a>
-  <a
-    class="btn"
-    aria-label="GitHub"
-    href="https://github.com/khui0"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <SimpleIconsGithub></SimpleIconsGithub>
-    github.com/khui0
-  </a>
-  <a
-    class="btn"
-    aria-label="Bluesky"
-    href="https://bsky.app/profile/kennyhui.dev"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <SimpleIconsBluesky></SimpleIconsBluesky>
-    @kennyhui.dev
-  </a>
-</section>
+    <p>Email</p>
+    <a data-nodrag href="mailto:hello@kennyhui.dev">hello@kennyhui.dev</a>
+  </section>
+  <section class="my-2 flex flex-wrap justify-center gap-2">
+    <a
+      class="btn"
+      aria-label="LinkedIn"
+      href="https://www.linkedin.com/in/k-hui/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <SimpleIconsLinkedin></SimpleIconsLinkedin>
+      linkedin.com/in/k-hui
+    </a>
+    <a
+      class="btn"
+      aria-label="GitHub"
+      href="https://github.com/khui0"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <SimpleIconsGithub></SimpleIconsGithub>
+      github.com/khui0
+    </a>
+    <a
+      class="btn"
+      aria-label="Bluesky"
+      href="https://bsky.app/profile/kennyhui.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <SimpleIconsBluesky></SimpleIconsBluesky>
+      @kennyhui.dev
+    </a>
+  </section>
+  <section class="text-base-content/30 text-center text-[11px] leading-tight">
+    <p>&copy; 2025 Kenny Hui</p>
+    <p>All Rights Reserved.</p>
+  </section>
+</div>
