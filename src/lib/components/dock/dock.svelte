5e29708ce1885ba1ad2e6e1b4fc357a39ac44f0d
Kenny <101839505+khui0@users.noreply.github.com>
2025-04-15 18:37:08 -0400

    feat: show non pinned dock items

--- src/lib/components/dock/dock.svelte
+++ src/lib/components/dock/dock.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { applications } from "$lib/applications.svelte";
+  import { windows } from "../window/windows.svelte";
   import DockItem from "./dock-item.svelte";
 </script>
 
@@ -9,6 +10,9 @@
     class="bg-dock flex min-h-15.5 w-fit max-w-full shrink-0 items-start gap-3 rounded-[18px] bg-[F6F6F6]/36 p-2.5 shadow-xl backdrop-blur-[135px]"
   >
     {#each applications.filter((app) => app.showInDock) as app}
+      <DockItem {app} />
+    {/each}
+    {#each applications.filter((app) => !app.showInDock && windows.findIndex( (window) => window.id.startsWith(app.id), ) !== -1) as app}
       <DockItem {app} />
     {/each}
   </div>
