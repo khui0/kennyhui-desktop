fff09824ba1c1737a91b8b73e30ebd71a449dcd1
Kenny <101839505+khui0@users.noreply.github.com>
2025-04-24 17:21:56 -0400

    fix: window resize hitboxes

--- src/lib/components/window/window.svelte
+++ src/lib/components/window/window.svelte
@@ -26,48 +26,48 @@
   <!-- Top -->
   <div
     data-window-transform="0,-1"
-    class="not-window-active:pointer-events-none z-[90] absolute -top-1.25 right-1.25 left-1.25 h-2.5 cursor-ns-resize"
+    class="not-window-active:pointer-events-none absolute -top-1.25 right-1.25 left-1.25 z-[100] h-2.5 cursor-ns-resize"
   ></div>
   <!-- Bottom -->
   <div
     data-window-transform="0,1"
-    class="not-window-active:pointer-events-none z-[90] absolute right-1.25 -bottom-1.25 left-1.25 h-2.5 cursor-ns-resize"
+    class="not-window-active:pointer-events-none absolute right-1.25 -bottom-1.25 left-1.25 z-[100] h-2.5 cursor-ns-resize"
   ></div>
   <!-- Left -->
   <div
     data-window-transform="-1,0"
-    class="not-window-active:pointer-events-none z-[90] absolute top-1.25 bottom-1.25 -left-1.25 w-2.5 cursor-ew-resize"
+    class="not-window-active:pointer-events-none absolute top-1.25 bottom-1.25 -left-1.25 z-[100] w-2.5 cursor-ew-resize"
   ></div>
   <!-- Right -->
   <div
     data-window-transform="1,0"
-    class="not-window-active:pointer-events-none z-[90] absolute top-1.25 -right-1.25 bottom-1.25 w-2.5 cursor-ew-resize"
+    class="not-window-active:pointer-events-none absolute top-1.25 -right-1.25 bottom-1.25 z-[100] w-2.5 cursor-ew-resize"
   ></div>
   <!-- Top Left -->
   <div
     data-window-transform="-1,-1"
-    class="not-window-active:pointer-events-none z-[90] absolute -top-1.25 -left-1.25 h-4 w-4 cursor-nwse-resize"
+    class="not-window-active:pointer-events-none absolute -top-1.25 -left-1.25 z-[100] h-4 w-4 cursor-nwse-resize"
   ></div>
   <!-- Top Right -->
   <div
     data-window-transform="1,-1"
-    class="not-window-active:pointer-events-none z-[90] absolute -top-1.25 -right-1.25 h-4 w-4 cursor-nesw-resize"
+    class="not-window-active:pointer-events-none absolute -top-1.25 -right-1.25 z-[100] h-4 w-4 cursor-nesw-resize"
   ></div>
   <!-- Bottom Left -->
   <div
     data-window-transform="-1,1"
-    class="not-window-active:pointer-events-none z-[90] absolute -bottom-1.25 -left-1.25 h-4 w-4 cursor-nesw-resize"
+    class="not-window-active:pointer-events-none absolute -bottom-1.25 -left-1.25 z-[100] h-4 w-4 cursor-nesw-resize"
   ></div>
   <!-- Bottom Right -->
   <div
     data-window-transform="1,1"
-    class="not-window-active:pointer-events-none z-[90] absolute -right-1.25 -bottom-1.25 h-4 w-4 cursor-nwse-resize"
+    class="not-window-active:pointer-events-none absolute -right-1.25 -bottom-1.25 z-[100] h-4 w-4 cursor-nwse-resize"
   ></div>
 {/snippet}
 
 <div
   class={{
-    "bg-window rounded-box @container fixed box-content overflow-hidden shadow-2xl shadow-black/20 transition-opacity [[data-window-active]]:shadow-black/40": true,
+    "bg-window rounded-box @container fixed box-content shadow-2xl shadow-black/20 transition-opacity [[data-window-active]]:shadow-black/40": true,
     "pointer-events-auto opacity-100": open,
     "pointer-events-none opacity-0": !open,
   }}
@@ -76,7 +76,7 @@
   data-window-transform="0,0"
   data-window-min="{minSize.x},{minSize.y}"
 >
-  <div class="relative flex h-full w-full flex-col overflow-hidden">
+  <div class="rounded-box relative flex h-full w-full flex-col overflow-hidden">
     {@render children()}
   </div>
   {@render handles()}
